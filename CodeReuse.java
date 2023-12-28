1ac3dad11abca148dc430d4c144f8aa7dfade211
mustuxcodes <122674263+mustuxcodes@users.noreply.github.com>
2023-12-28 18:00:31 +0500

    Create CodeReuse.java

---
+++ CodeReuse.java
@@ -0,0 +1,93 @@
+import java.util.Arrays;
+
+public class CodeReuse {
+    
+    /* working is written below in cmnts
+     * Sorts an array of integers in descending order.
+     */
+    public static void sortDescending(int[] array) {
+        boolean swapped;
+        for (int i = 0; i < array.length - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] < array[j + 1]) {
+                    // swap array[j] and array[j + 1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
+
+//Question 4 its working is written below chck out below
+/*
+     * Performs matrix multiplication on two matrices.
+     * @param matrixA First matrix
+     * @param matrixB Second matrix
+     * @param rowsA Number of rows in matrix A
+     * @param columnsA Number of columns in matrix A
+     * @param columnsB Number of columns in matrix B
+     * @return The resulting matrix after multiplication
+     */
+
+
+public static double[] calculateStatistics(int[] array) {
+    Arrays.sort(array);
+    double[] stats = new double[4];
+    stats[0] = calculateMedian(array);
+    stats[1] = calculateVariance(array);
+    stats[2] = Math.sqrt(stats[1]); // Standard Deviation
+    stats[3] = calculateSumOfSquares(array);
+    return stats;
+}
+
+private static double calculateMedian(int[] array) {
+    int middle = array.length / 2;
+    if (array.length % 2 == 1) {
+        return array[middle];
+    } else {
+        return (array[middle - 1] + array[middle]) / 2.0;
+    }
+}
+
+private static double calculateVariance(int[] array) {
+    double mean = Arrays.stream(array).average().orElse(Double.NaN);
+    return Arrays.stream(array)
+                 .mapToDouble(num -> Math.pow(num - mean, 2))
+                 .average().orElse(Double.NaN);
+}
+
+private static double calculateSumOfSquares(int[] array) {
+    return Arrays.stream(array)
+                 .mapToDouble(num -> num * num)
+                 .sum();
+}
+
+//Question 6 
+
+/*
+     * Performs matrix multiplication on two matrices.
+     * @param matrixA First matrix
+     * @param matrixB Second matrix
+     * @param rowsA Number of rows in matrix A
+     * @param columnsA Number of columns in matrix A
+     * @param columnsB Number of columns in matrix B
+     * @return The resulting matrix after multiplication
+     */
+
+public static int[][] matrixMultiplication(int[][] matrixA, int[][] matrixB, int rowsA, int columnsA, int columnsB) {
+    int[][] product = new int[rowsA][columnsB];
+    for (int i = 0; i < rowsA; i++) {
+        for (int j = 0; j < columnsB; j++) {
+            for (int k = 0; k < columnsA; k++) {
+                product[i][j] += matrixA[i][k] * matrixB[k][j];
+            }
+        }
+    }
+    return product;
+}
+}
